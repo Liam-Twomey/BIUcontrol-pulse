212896ad42982cf35d21f5e92262a1b60aca8288
Hydrogenase <lptwomey@ucdavis.edu>
2023-07-17 12:38:06 -0700

    works on Rpi4B w/o errors, have not tested func

--- BIUgui.py
+++ BIUgui.py
@@ -11,7 +11,7 @@ def startprocess():
     spraytime        = str(float(stime.value)/1000)
     retractiondelay  = str(float(rdelay.value)/1000)
     plungedelay      = str(float(pdelay.value)/1000)
-    arguments = ["python3","BIUapplyandplunge.py","--pulse",False,"--stime",spraytime,"--rdelay",retractiondelay,"--pdelay",plungedelay]
+    arguments = ["python3","BIUapplyandplunge.py","--pulse",'False',"--stime",spraytime,"--rdelay",retractiondelay,"--pdelay",plungedelay]
     if donotplunge.value==1:
         arguments.append("--donotplunge")
     call(arguments)
@@ -23,6 +23,7 @@ def powerup():
     call(arguments)
     try:
         button_start.enable()
+        button_pulse.enable()
     except:
         return
 def powerdown():
@@ -31,17 +32,16 @@ def powerdown():
     call(arguments)
     try:
         button_start.disable()
+        button_pulse.disable()
     except:
         return
     
 def pulsestartprocess():
     print("Starting pulse spray.")
-    spraytime        = str(float(stime.value)/1000)
     retractiondelay  = str(float(rdelay.value)/1000)
     plungedelay      = str(float(pdelay.value)/1000)
-    pulsecount       = str(float(pnum.value)/1000)
     pulselength       = str(float(plen.value)/1000)
-    arguments = ["python3","BIUapplyandplunge.py","--pulse",True,"--pulseno",pulsecount,"--pulselen",pulselength,"--rdelay",retractiondelay,"--pdelay",plungedelay]
+    arguments = ["python3","BIUapplyandplunge.py","--pulse",'True',"--pcycles",pnum.value,"--stime",pulselength, "--breaktime", pinterval, "--rdelay",retractiondelay,"--pdelay",plungedelay]
     if donotplunge.value==1:
         arguments.append("--donotplunge")
     call(arguments)
@@ -57,11 +57,11 @@ def cleanprocess():
     Popen(arguments)
     #call(["python3","cleancontrol.py","--stime",stime,"--cycles",cycles])
 
-def pedal():
-    GPIO.setup(pin.pedalsensor,GPIO.IN, pull_up_down = GPIO.PUD_UP)
-    if button_start.enabled and GPIO.input(pin.pedalsensor)==0:
-        print("Pedal triggered")
-        startprocess()
+# def pedal():
+#     GPIO.setup(pin.pedalsensor,GPIO.IN, pull_up_down = GPIO.PUD_UP)
+#     if button_start.enabled and GPIO.input(pin.pedalsensor)==0:
+#         print("Pedal triggered")
+#         startprocess()
 
 def text_box(disp:str, position:list, default):
     '''
@@ -89,20 +89,22 @@ pdelaylabel, pdelay = text_box('Plunge delay (ms):',     position = [0,3], defau
 pulselabel  = Text(app, text="(Anti) Pulse Spray", grid=[0,4,2,1], color='white', bg='dim gray')
 plen_label, plen   = text_box('Pulse length (ms):',   position = [0,5], default = 16)
 pnum_label, pnum   = text_box('Pulse count:',   position = [0,6], default = 5)
-pulsenote            = Text(app, text="(Uses retraction & plunge settings from std.)", grid=[0,7,2,1])
+pint_label, pint   = text_box('Pulse interval:',   position = [0,7], default = 20)
+pulsenote            = Text(app, text="(Uses retraction & plunge settings from std.)", grid=[0,8,2,1])
 ## Buttons
-button_title = Text(app, text="Triggers", grid=[0,8,2,1], color='white', bg='dim grey')
-donotplunge = CheckBox(  app,                       text="Dry fire (do not plunge)?",   grid=[0,9,2,1], align='left')
-button_up   = PushButton(app, command=powerup,      text="        Ready        ", grid=[0,10])
-button_pulse= PushButton(app, command=pulsestartprocess, text="Pulse & Plunge",  grid=[1,10])
-button_start= PushButton(app, command=startprocess, text="Spray & Plunge",  grid=[0,11])
-button_down = PushButton(app, command=powerdown,    text="         Abort         ",   grid=[1,11], align='left')
+button_title = Text(app, text="Triggers", grid=[0,9,2,1], color='white', bg='dim grey')
+donotplunge = CheckBox(  app,                       text="Dry fire (do not plunge)?",   grid=[0,10,2,1], align='left')
+button_up   = PushButton(app, command=powerup,      text="        Ready        ", grid=[0,11])
+button_pulse= PushButton(app, command=pulsestartprocess, text="Pulse & Plunge",  grid=[1,11])
+button_start= PushButton(app, command=startprocess, text="Spray & Plunge",  grid=[0,12])
+button_down = PushButton(app, command=powerdown,    text="         Abort         ",   grid=[1,12], align='left')
 
 button_up.bg="lime green"
 button_start.bg = (255, 50, 50)
 button_down.bg = "orange"
 button_pulse.bg = 'violet'
 button_start.disable()
+button_pulse.disable()
 # Cleaning
 cleanlabel    = Text(app, text="Cleaning settings:", grid=[2,0,2,1], color='white', bg='dim gray')
 cleancycleslabel, cleancycles = text_box('Cleaning cycles:',     position = [2,1], default = 5)
@@ -114,12 +116,12 @@ button_clean.bg = "lightblue"
 for element in [button_up, button_start, donotplunge, button_down, button_clean, button_pulse]:
     element.text_size = 12
 
-#GPIO.setwarnings(False)
-#GPIO.setmode(GPIO.BCM)
-##app.repeat(100,pedal)
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BCM)
+# app.repeat(100,pedal)
 app.display()
 
 #shutdown
 print('BIU program shutting down...')
 powerdown()
-GPIO.cleanup()
+# GPIO.cleanup()
