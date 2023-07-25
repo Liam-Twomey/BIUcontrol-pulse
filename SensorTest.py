c5158bb49538cc803d2152851d65227e4d3c450f
Hydrogenase <lptwomey@ucdavis.edu>
2023-07-25 13:07:29 -0700

    Fixed for running on RPi

--- SensorTest.py
+++ SensorTest.py
@@ -3,15 +3,14 @@ import BIUpinlist as pin
 
 pins = [2,3,4,17,27,22,10,9,11,5,6,13,19,26,14,15,18,23,24,8,7,12,16,20,21]#[13,19,26,16,20]
 
-GPIO.setmode(GPIO.BCM)
-for i in pins:
-    GPIO.setup(i,GPIO.OUT)
 def hi(pinnum):
-    for i in pinnum:
-        GPIO.output(i,GPIO.HIGH)
+    GPIO.output(pinnum,GPIO.HIGH)
 def lo(pinnum):
-    for i in pinnum:
-        GPIO.output(i,GPIO.LOW)
+    GPIO.output(pinnum,GPIO.LOW)
 def reset():
     GPIO.cleanup()
- 
+def init():
+    GPIO.setmode(GPIO.BCM)
+    for i in pins:
+        GPIO.setup(i,GPIO.OUT)
+init()
