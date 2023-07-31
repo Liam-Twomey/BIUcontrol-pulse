2dd8d82489f33c91936b2439d5fabb5d90537f03
paleothefish <akuan@ucdavis.edu>
2023-07-30 22:05:04 -0700

    separate out GUI callback functions from BIUgui.py to make the code more readable and avoid the use of global variables

---
+++ BIU_gui_callback_functions.py
@@ -0,0 +1,57 @@
+from guizero import App, TextBox, Text, PushButton, CheckBox
+from subprocess import call, Popen
+
+
+def startprocess(stime, rdelay, pdelay, is_dry_fire:bool):
+    '''
+    This function takes in spraytime, retraction delay, and plunge delay to run BIUA&P in the system command line.
+    :return: void
+    '''
+    print("Starting continous spray process.")
+    spraytime        = str(float(stime.value)/1000)
+    retractiondelay  = str(float(rdelay.value)/1000)
+    plungedelay      = str(float(pdelay.value)/1000)
+    arguments = ["python3","BIUapplyandplunge.py","--pulse",'False',"--stime",spraytime,"--rdelay",retractiondelay,"--pdelay",plungedelay]
+    if is_dry_fire:
+        arguments.append("--donotplunge")
+    call(arguments)
+    
+def powerup(buttons_list):
+    print("Power up")
+    arguments = ["python3","BIUpowerupdown.py","--updown","up"]
+    call(arguments)
+    try:
+        for button in buttons_list:
+            button.enable()
+    except:
+        return
+    
+def powerdown(buttons_list):
+    print("Power down")
+    arguments = ["python3","BIUpowerupdown.py","--updown","down"]
+    call(arguments)
+    try:
+        for button in buttons_list:
+            button.disable()
+    except:
+        return
+    
+def pulsestartprocess(rdelay, pdelay, plen, is_dry_fire):
+    print("Starting pulse spray.")
+    retractiondelay  = str(float(rdelay.value)/1000)
+    plungedelay      = str(float(pdelay.value)/1000)
+    pulselength       = str(float(plen.value)/1000)
+    arguments = ["python3","BIUapplyandplunge.py","--pulse",'True',"--pcycles",pnum.value,"--stime",pulselength, "--breaktime", pinterval, "--rdelay",retractiondelay,"--pdelay",plungedelay]
+    if (is_dry_fire):
+        arguments.append("--donotplunge")
+    call(arguments)
+
+def cleanprocess(cleantime, cleancycles):
+    print("Starting clean process")
+    spraytime  = str(float(cleantime.value)/1000)
+    cycles = cleancycles.value
+    arguments = ["python3","BIUclean.py","--stime",spraytime,"--cycles",cycles]
+    #print(arguments)
+    #call(arguments)
+    Popen(arguments)
+    #call(["python3","cleancontrol.py","--stime",stime,"--cycles",cycles])
--- BIUgui.py
+++ BIUgui.py
@@ -1,65 +1,11 @@
 #!/usr/bin/env python3
 
 from guizero import App, TextBox, Text, PushButton, CheckBox
-from subprocess import call, Popen
-#import RPi.GPIO as GPIO
-import gpio as GPIO
+import RPi.GPIO as GPIO
+#import gpio as GPIO
 import BIUpinlist as pin
 
-def startprocess():
-    '''
-    This function takes in spraytime, retraction delay, and plunge delay to run BIUA&P in the system command line.
-    :return: void
-    '''
-    print("Starting spray process.")
-    spraytime        = str(float(stime.value)/1000)
-    retractiondelay  = str(float(rdelay.value)/1000)
-    plungedelay      = str(float(pdelay.value)/1000)
-    arguments = ["python3","BIUapplyandplunge.py","--pulse",'False',"--stime",spraytime,"--rdelay",retractiondelay,"--pdelay",plungedelay]
-    if donotplunge.value==1:
-        arguments.append("--donotplunge")
-    call(arguments)
-    button_start.disable()
-    
-def powerup():
-    print("Power up")
-    arguments = ["python3","BIUpowerupdown.py","--updown","up"]
-    call(arguments)
-    try:
-        button_start.enable()
-        button_pulse.enable()
-    except:
-        return
-def powerdown():
-    print("Power down")
-    arguments = ["python3","BIUpowerupdown.py","--updown","down"]
-    call(arguments)
-    try:
-        button_start.disable()
-        button_pulse.disable()
-    except:
-        return
-    
-def pulsestartprocess():
-    print("Starting pulse spray.")
-    retractiondelay  = str(float(rdelay.value)/1000)
-    plungedelay      = str(float(pdelay.value)/1000)
-    pulselength       = str(float(plen.value)/1000)
-    arguments = ["python3","BIUapplyandplunge.py","--pulse",'True',"--pcycles",pnum.value,"--stime",pulselength, "--breaktime", pinterval, "--rdelay",retractiondelay,"--pdelay",plungedelay]
-    if donotplunge.value==1:
-        arguments.append("--donotplunge")
-    call(arguments)
-    button_start.disable()
-
-def cleanprocess():
-    print("Starting clean process")
-    spraytime  = str(float(cleantime.value)/1000)
-    cycles = cleancycles.value
-    arguments = ["python3","BIUclean.py","--stime",spraytime,"--cycles",cycles]
-    #print(arguments)
-    #call(arguments)
-    Popen(arguments)
-    #call(["python3","cleancontrol.py","--stime",stime,"--cycles",cycles])
+from BIU_gui_callback_functions import *
 
 # def pedal():
 #     GPIO.setup(pin.pedalsensor,GPIO.IN, pull_up_down = GPIO.PUD_UP)
@@ -67,10 +13,11 @@ def cleanprocess():
 #         print("Pedal triggered")
 #         startprocess()
 
-def text_box(disp:str, position:list, default):
+def text_box(app, disp:str, position:list, default):
     '''
-    Makes a label+textbox pair
-
+    Takes in app as zerogui application object, and then return the following to the caller of this function:
+        label: a zerogui Text object
+        box : a zerogui TextBox object
     '''
     if len(position) == 2:
         posbox = [position[0]+1, position[1]]
@@ -82,50 +29,60 @@ def text_box(disp:str, position:list, default):
     box.text_size = 12
     return label, box
 
+def main():
+    app = App(title="Back-it-up", layout="grid", width = 600, height = 330)
     
-app = App(title="Back-it-up", layout="grid", width = 600, height = 450)
-# Standard Spray
-stdlabel            = Text(app, text="Standard Spray", color='white', grid=[0,0,2,1], bg = 'dim gray')
-stimelabel, stime   = text_box('Spray time (ms):',       position = [0,1], default = 30)
-rdelaylabel, rdelay = text_box('Retraction delay (ms):', position = [0,2], default = 50)
-pdelaylabel, pdelay = text_box('Plunge delay (ms):',     position = [0,3], default = 50)
-# Pulse Spray
-pulselabel  = Text(app, text="(Anti) Pulse Spray", grid=[0,4,2,1], color='white', bg='dim gray')
-plen_label, plen   = text_box('Pulse length (ms):',   position = [0,5], default = 16)
-pnum_label, pnum   = text_box('Pulse count:',   position = [0,6], default = 5)
-pint_label, pint   = text_box('Pulse interval (ms):',   position = [0,7], default = 20)
-pulsenote            = Text(app, text="(Uses retraction & plunge settings from std.)", grid=[0,8,2,1])
-## Buttons
-button_title = Text(app, text="Triggers", grid=[0,9,2,1], color='white', bg='dim grey')
-donotplunge = CheckBox(  app,                       text="Dry fire (do not plunge)?",   grid=[0,10,2,1], align='left')
-button_up   = PushButton(app, command=powerup,      text="        Ready        ", grid=[0,11])
-button_pulse= PushButton(app, command=pulsestartprocess, text="Pulse & Plunge",  grid=[1,11])
-button_start= PushButton(app, command=startprocess, text="Spray & Plunge",  grid=[0,12])
-button_down = PushButton(app, command=powerdown,    text="         Abort         ",   grid=[1,12], align='left')
+    # GUI for Standard Spray parameters entries
+    stdlabel            = Text(app, text="Standard Spray", color='white', grid=[0,0,2,1], bg = 'dim gray')
+    stimelabel, stime   = text_box(app, 'Spray time (ms):',       position = [0,1], default = 30)
+    rdelaylabel, rdelay = text_box(app, 'Retraction delay (ms):', position = [0,2], default = 50)
+    pdelaylabel, pdelay = text_box(app, 'Plunge delay (ms):',     position = [0,3], default = 50)
 
-button_up.bg="lime green"
-button_start.bg = (255, 50, 50)
-button_down.bg = "orange"
-button_pulse.bg = 'violet'
-button_start.disable()
-button_pulse.disable()
-# Cleaning
-cleanlabel    = Text(app, text="Cleaning settings:", grid=[2,0,2,1], color='white', bg='dim gray')
-cleancycleslabel, cleancycles = text_box('Cleaning cycles:',     position = [2,1], default = 5)
-cleantimelabel, cleantime = text_box('Clean pulse length (ms):', position = [2,2], default = 200)
-## Buttons
-button_clean = PushButton(app, command=cleanprocess, text="        Clean        ", grid=[2,3,2,1])
-button_clean.bg = "lightblue"
+    # GUI for Pulse Spray parameters entries
+    pulselabel  = Text(app, text="(Anti) Pulse Spray", grid=[0,4,2,1], color='white', bg='dim gray')
+    plen_label, plen   = text_box(app, 'Pulse length (ms):',   position = [0,5], default = 16)
+    pnum_label, pnum   = text_box(app, 'Pulse count:',   position = [0,6], default = 5)
+    pint_label, pint   = text_box(app, 'Pulse interval (ms):',   position = [0,7], default = 20)
+    pulsenote            = Text(app, text="(Uses retraction & plunge settings from std.)", grid=[0,8,2,1])
 
-for element in [button_up, button_start, donotplunge, button_down, button_clean, button_pulse]:
-    element.text_size = 12
+    ## Buttons, commands are defined in BIU_gui_callback_functions.py
+    button_title = Text(master=app, text="Triggers", grid=[0,9,2,1], color='white', bg='dim grey')
+    donotplunge = CheckBox(master=app, text="Dry fire (do not plunge)?",   grid=[0,10,2,1], align='left')
 
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)
-# app.repeat(100,pedal)
-app.display()
+    button_pulse= PushButton(master=app, text="Pulse & Plunge", grid=[2,11], align='left', command=pulsestartprocess, args = [rdelay, pdelay, plen, donotplunge.value==1])
+    button_pulse.disable()
+    button_pulse.bg = 'violet'
 
-#shutdown
-print('BIU program shutting down...')
-powerdown()
-# GPIO.cleanup()
+    button_start= PushButton(master=app, text="Spray & Plunge", grid=[1,11], align='left', command=startprocess, args=[stime, rdelay, pdelay, donotplunge.value==1])
+    button_start.bg = (255, 50, 50)
+    button_start.disable()
+
+    button_up   = PushButton(master=app, text="  Ready  ", grid=[0,11], align='left', command=powerup, args = [[button_start, button_pulse]])
+    button_up.bg="lime green"
+
+    button_down = PushButton(master=app, text="  Abort  ", grid=[3,11], align='left', command=powerdown, args = [[button_start, button_pulse]])
+    button_down.bg = "orange"
+
+
+    # Cleaning
+    cleanlabel    = Text(app, text="Cleaning settings:", grid=[2,0,2,1], color='white', bg='dim gray')
+    cleancycleslabel, cleancycles = text_box(app, 'Cleaning cycles:',     position = [2,1], default = 5)
+    cleantimelabel, cleantime = text_box(app, 'Clean pulse length (ms):', position = [2,2], default = 200)
+    ## Buttons
+    button_clean = PushButton(master = app, text=" Clean ", grid=[2,3,2,1], command=cleanprocess, args = [cleantime, cleancycles])
+    button_clean.bg = "lightblue"
+
+    for element in [button_up, button_start, donotplunge, button_down, button_clean, button_pulse]:
+        element.text_size = 12
+
+    # GPIO.setwarnings(False)
+    # GPIO.setmode(GPIO.BCM)
+    # app.repeat(100,pedal)
+    app.display()
+
+    #shutdown
+    print('BIU program shutting down...')
+    powerdown([button_start, button_pulse])
+    # GPIO.cleanup()
+    
+main()
