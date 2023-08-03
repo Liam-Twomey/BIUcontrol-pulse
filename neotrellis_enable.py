5cc61a43d7f487062b7c67f30012dd09b7cc35ca
Allison Kuan <akuan@ucdavis.edu>
2023-08-03 12:46:40 -0700

    doing some more tests?

--- neotrellis_enable.py
+++ neotrellis_enable.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import time
 import board
 from adafruit_neotrellis.neotrellis import NeoTrellis
@@ -5,6 +7,14 @@ from adafruit_neotrellis.neotrellis import NeoTrellis
 # create the i2c object for the trellis
 i2c_bus = board.I2C()  # uses board.SCL and board.SDA
 # i2c_bus = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+
+# create a callback function
+def key_event(event):
+    if event.number in range(6):
+        if event.edge == NeoTrellis.EDGE_RISING:
+            trellis.pixels[event.number] = key_colors[event.number]
+        elif event.edge == NeoTrellis.EDGE_FALLING:
+            trellis.pixels[event.number] = (0, 0, 0)
 
 # create the trellis
 trellis = NeoTrellis(i2c_bus)
@@ -19,37 +29,14 @@ ORANGE = (255, 128, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
-WHITE = (100, 0, 0)
+WHITE = (255, 255, 255
 
-
-# this will be called when button events are received
-def blink(event):
-    # turn the LED on when a rising edge is detected
-    if event.edge == NeoTrellis.EDGE_RISING:
-        trellis.pixels[event.number] = ORANGE
-    # turn the LED off when a falling edge is detected
-    elif event.edge == NeoTrellis.EDGE_FALLING:
-        trellis.pixels[event.number] = OFF
-
-
-for i in range(16):
-    # activate rising edge events on all keys
+# activate rising and falling edge events on all keys
+for i in range(6):
     trellis.activate_key(i, NeoTrellis.EDGE_RISING)
-    # activate falling edge events on all keys
     trellis.activate_key(i, NeoTrellis.EDGE_FALLING)
-    # set all keys to trigger the blink callback
-    trellis.callbacks[i] = blink
-
-    # cycle the LEDs on startup
-    trellis.pixels[i] = PURPLE
-    time.sleep(0.05)
-
-for i in range(16):
-    trellis.pixels[i] = OFF
-    time.sleep(0.05)
+    trellis.callbacks[i] = key_event
 
 while True:
-    # call the sync function call any triggered callbacks
+    # call the sync function to update the trellis state
     trellis.sync()
-    # the trellis can only be read every 17 millisecons or so
-    time.sleep(0.02)
